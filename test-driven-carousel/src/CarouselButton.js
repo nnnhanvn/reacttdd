d44628ac6fca9857e5d032a438a80880085e22dc
nguyen.ngoc-nhan <nnnhanvn@gmail.com>
2020-06-29 15:25:00 +0700

    config jest

---
+++ test-driven-carousel/src/CarouselButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const CarouselButton = (props) => <button {...props} />;
+
+CarouselButton.propTypes = { children: PropTypes.node.isRequired };
+
+export default CarouselButton;
