34f7416dc949302757b501ae9ea8268052f76c41
nguyen.ngoc-nhan <nnnhanvn@gmail.com>
2020-07-01 09:10:46 +0700

    configure webpack

---
+++ test-driven-carousel/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: "development",
+
+    entry: {
+        carousel: "./src/Carousel.js",
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: require.resolve("babel-loader"),
+            },
+        ],
+    },
+};
